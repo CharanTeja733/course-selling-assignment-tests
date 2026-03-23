33bf6b0e5f2c3c3a15e8266c33e37501bd12feac
CharanTeja733 <charanduvvuru.733@gmail.com>
2026-03-23 17:34:27 +0530

    make small change

--- src/routes/lessons.routes.ts
+++ src/routes/lessons.routes.ts
@@ -3,7 +3,8 @@ import { createLessonController } from "../controllers/lessons.controllers";
 import { requireRole } from "../middlewares/authorization.middlewares";
 
 const router = express.Router();
-// 
+
+
 router.use(requireRole('INSTRUCTOR'));
 router.post('/', createLessonController);
 
